907142c2dbbc2be8c736f0885fedf3283957df9b
Vlad <shcheglovvladislav@mail.ru>
2025-02-13 22:43:33 +0300

    some fix

--- activate.py
+++ activate.py
@@ -6,28 +6,62 @@ import pyaudio
 import whisper
 import numpy as np
 import torch
-from vosk import Model,KaldiRecognizer
+import whisper
+import librosa
+from docx import Document
+import subprocess
+import aspose.words as aw
 
+
+
+word_path = "C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.EXE"
 # model = whisper.load_model('medium')
-model = Model("vosk-model-en-us-0.22")
+model = whisper.load_model("medium")
 recognizer = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print('Скажите что-нибудь!')
-    audio = recognizer.listen(source)
-    audio_data = np.frombuffer(audio.get_raw_data(), dtype=np.int16)
-    audio_data = audio_data.astype(np.float32) / 32768.0 # нормализация до [-1 1]
-    
-    try:
-        text = model.transcribe(audio_data)
-        result = text["text"].strip().lower()
-        print(f"Вы сказали: {result}")
 
-    
-        if 'открой браузер' in text:
-            webbrowser.open("https://www.google.com")
-        elif "как дела" in text:
-            print("У меня всё отлично, спасибо!") 
-            
-    except sr.UnknownValueError:
-        print("Извините, я не смог распознать вашу речь.")  # Обработка ошибки распознавания
+def movement(command):
+    if 'открой браузер' in command:
+        webbrowser.open("https://www.google.com")
+    elif "как дела" in command:
+        print("У меня всё отлично, спасибо!")
+    elif "cоздай документ" in command:
+        doc = Document()
+        doc.add_paragraph(command)
+        doc.save("new.docx")
+        print("Документ успешно создан!")
+
+    elif "открой документ" in command:
+        try:
+            subprocess.Popen([word_path])
+            print("Microsoft Word успешно запущен.")
+        except FileNotFoundError:
+            print("Не удалось найти Microsoft Word по указанному пути.")
+
+def record():
+    with sr.Microphone() as source:
+        print('Скажите что-нибудь!')
+        audio = recognizer.listen(source)
+            # Сохранение аудио в файл
+        with open("speech.wav", "wb") as f:
+            f.write(audio.get_wav_data())
+
+        try:
+            audio, srate = librosa.load("speech.wav", sr=16000)
+            result = model.transcribe(audio,language = "ru")
+            text = result["text"].strip().lower()
+            print(f"Вы сказали: {text}")
+
+
+        except sr.UnknownValueError:
+            print("Извините, я не смог распознать вашу речь.")  # Обработка ошибки распознавания  
+        return text  
+
+
+while True:
+    command = record()
+
+    movement(command=command)
+
+
+
